ee38b305c08d4a8666e979796c8a291d75b0b799
agustinrios <agusrios_16@hotmail.com>
2021-06-29 12:17:19 -0300

    modelo de busqueda

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const { leerInput } = require('./helpers/inquirer');
+const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async() => {
-    const texto = await leerInput('hola: ');
 
-    console.log(texto);
+    const busquedas = new Busquedas();
+    let opt;
+
+    do {
+
+        opt = await inquirerMenu();
+
+        switch(opt){
+            case 1:
+
+            const lugar = await leerInput('Ciudad: ');
+
+            console.log('\nInformación de la ciudad\n'.green);
+            console.log('Ciudad:',);
+            console.log('Lat:',);
+            console.log('Lng:', );
+            console.log('Temperatura:');
+            console.log('Maxima:');
+            console.log('Minima:');
+            break;
+        }
+
+        if(opt !== 0) await pausa();
+
+    }while( opt !== 0);
 }
 
 main();
---
+++ models/busquedas.js
@@ -0,0 +1,19 @@
+
+
+class Busquedas{
+
+    historial = ['mendoza', 'cordoba', 'concepcion del uruguay'];
+
+    constructor(){
+
+    }
+
+    async ciudad(lugar = '') {
+        
+        console.log(lugar);
+
+        return[];
+    }
+}
+
+module.exports = Busquedas;
